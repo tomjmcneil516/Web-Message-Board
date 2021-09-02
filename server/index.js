710b6d536b750aed4ba7666bed841c8abe1c283e
Tom <tommyxwe@gmail.com>
2021-09-01 23:44:28 -0700

    constructed db, handled request with response

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 
 const app = express();
+
+
+//database stuff
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "webapp"
+});
+
+
+
+
+//database stuff end
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -12,23 +30,42 @@ app.get('/client/index.html', (req,res) => {
     });
 });
 
-function isValidMessage(message){
+function isValidMessage(message){ //validation on server side
     return message.name && message.name.toString().trim() !== '' &&
-    message.content && message.content.toString().trim() !== '';
+    message.content && message.content.toString().trim() !== '' &&
+    message.content.toString().trim().length < 255;
 }
+
+function insertMessage(message){
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        const sqlcmd = "INSERT INTO messages (name, content, date) VALUES ('" + message.name + "', '" + message.content + "', '" + message.date + "')";
+        con.query(sqlcmd, function (err, result) {
+          if (err) throw err;
+          console.log("1 record inserted");
+        });
+      });
+}
+
 
 app.post('/message', (req, res) => {
     if(isValidMessage(req.body)) {
         //insert into db
-        const message = { //validation on server side
+        const message = { 
             name: req.body.name.toString(),
-            content: req.body.content.toString()
+            content: req.body.content.toString(),
+            date: new Date().toDateString() + " " + new Date().toLocaleTimeString('en-US')
         };
-    console.log(message);
+
+        insertMessage(message);
+        res.json(message);
+        
+
     } else{
         res.status(422);
         rest.json({
-            message: "Error: No Content Submitted"
+            message: "Error: Invalid Submission"
         });
     }
 });
